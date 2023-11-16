9350794d2e6bda86bcea0b9d9ad665c98cab284a
vikasAjiledone <vikas.pandey@ajiledone.com>
2023-11-16 15:17:27 +0530

    installing router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import SignUp from './pages/SignUp'
+import React from "react";
+import SignUp from "./pages/SignUp";
 // import Dashboard from './pages/dashborad/Dashborad'
-import DashBoard from './pages/DashBoard'
+import DashBoard from "./pages/DashBoard.jsx";
+import { Routes, Route } from "react-router-dom";
+import ProjectCompletionDocumentation from "./pages/postconstruction/ProjectCompletionDocumentation";
 
 function App() {
-
   return (
     <>
-     {/* <SignUp/> */}
-     <DashBoard/>
+      <Routes>
+        <Route path="/" exact element={<DashBoard />} />
+        <Route path="/SignUp" exact element={<SignUp />} />
+        <Route
+          path="/ProjectCompletionDocumentation"
+          exact
+          element={<ProjectCompletionDocumentation />}
+        />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
